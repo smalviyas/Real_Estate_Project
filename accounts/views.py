31d8a06ae79e6f7580c9dab4b56c0988dc0a392b
pragtism <pragtism@gmail.com>
2022-05-25 15:04:40 +0530

    updated req.txt

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
 from email import message
+from multiprocessing import context
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
+
+from contacts.models import Contact
 
 def register(request):
     if request.method == 'POST':
@@ -69,4 +72,8 @@ def logout(request):
         return redirect('index')
 
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html') 
+    user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+    context = {
+        'user_contacts' : user_contacts
+    }
+    return render(request, 'accounts/dashboard.html', context) 
